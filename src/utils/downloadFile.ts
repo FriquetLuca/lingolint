2c544c8e39ae6739688c09cc45db0bce6bbea5b0
FriquetLuca <friquet.luca@gmail.com>
2026-03-23 03:42:17 +0100

    add new dependencies for icons and zip

---
+++ src/utils/downloadFile.ts
@@ -0,0 +1,19 @@
+export function downloadFile(
+  fileName: string,
+  fileContent: BlobPart[],
+  contentType = 'text/text'
+) {
+  const blob = new Blob(fileContent, { type: contentType });
+  downloadBlob(fileName, blob);
+}
+
+export function downloadBlob(fileName: string, blob: Blob) {
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = fileName;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+}
